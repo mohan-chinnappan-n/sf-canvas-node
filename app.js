55ce39ba86d3bb05f02cb72e2c8f9c7a6b89210d
Mohan Chinnappan <mohan.chinnappan.n@gmail.com>
2019-04-14 13:44:48 -0400

    contact records added

--- app.js
+++ app.js
@@ -8,15 +8,16 @@
 var express = require('express'),
   bodyParser = require('body-parser'),
   path = require('path'),
+  request = require('request'),
   // CryptoJS = require("crypto-js"),
   decode = require('salesforce-signed-request')
-;
+  ;
 var app = express();
 // make sure to set by:
 //  heroku config:set CANVAS_CONSUMER_SECRET=adsfadsfdsfsdafsdfsdf
 
 var consumerSecret = process.env.CANVAS_CONSUMER_SECRET;
- 
+
 app.use(express.static(path.join(__dirname, 'views')));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
@@ -30,22 +31,36 @@ app.get('/', function (req, res) {
 app.post('/signedrequest', function (req, res) {
 
   var signedRequest = decode(req.body.signed_request, consumerSecret),
-        context = signedRequest.context,
-        oauthToken = signedRequest.client.oauthToken,
-        instanceUrl = signedRequest.client.instanceUrl
-        ;
+    context = signedRequest.context,
+    oauthToken = signedRequest.client.oauthToken,
+    instanceUrl = signedRequest.client.instanceUrl
+    ;
 
-  var payload = {
-    instanceUrl: instanceUrl, 
+  const query = "SELECT Id, FirstName, LastName, Phone, Email FROM Contact";
+
+  contactRequest = {
+    url: instanceUrl + '/services/data/v45.0/query?q=' + query,
     headers: {
+      'Authorization': 'OAuth ' + oauthToken
+    }
+  };
+
+  request(contactRequest, function (err, response, body) {
+    const contactRecords = JSON.parse(body).records;
+    res.render('index', { context: context, imgTag: imgTag });
+
+    var payload = {
+      instanceUrl: instanceUrl,
+      headers: {
         'Authorization': 'OAuth ' + oauthToken
-    },
-    context: context 
-   };
-   res.render('index', { payload: payload });
+      },
+      context: context,
+      contacts: contactRecords
+    };
+    res.render('index', { payload: payload });
 
   });
 
-var port = process.env.PORT || 9000;
-app.listen(port);
-console.log('Listening on port ' + port);
+  var port = process.env.PORT || 9000;
+  app.listen(port);
+  console.log('Listening on port ' + port);
