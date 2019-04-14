6eb2df452d311ca86646f1dbe523269dd72ac6eb
Mohan Chinnappan <mohan.chinnappan.n@gmail.com>
2019-04-14 13:25:38 -0400

    node_modules removed

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
-// https://rajvakati.com/2017/10/19/salesforce-node-js-canvas-signed-request/
-// https://github.com/ccoenraets/salesforce-canvas-demo
+/*
+  app.js
+  mohan chinnappan
+    
+  Ref: https://github.com/ccoenraets/salesforce-canvas-demo
+
+  */
 var express = require('express'),
   bodyParser = require('body-parser'),
   path = require('path'),
-  CryptoJS = require("crypto-js"),
+  // CryptoJS = require("crypto-js"),
   decode = require('salesforce-signed-request')
 ;
 var app = express();
+// make sure to set by:
+//  heroku config:set CANVAS_CONSUMER_SECRET=adsfadsfdsfsdafsdfsdf
+
 var consumerSecret = process.env.CANVAS_CONSUMER_SECRET;
  
 app.use(express.static(path.join(__dirname, 'views')));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ entended: true }));
+// just a welcome page
 app.get('/', function (req, res) {
   res.render('welcome');
 });
 
+// SF call POST us on this URI with signed request
 app.post('/signedrequest', function (req, res) {
 
   var signedRequest = decode(req.body.signed_request, consumerSecret),
